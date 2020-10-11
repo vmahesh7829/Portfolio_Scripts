996451f29876ccd9ac86a9ff6c07e74db7de72ab
vmahesh7829 <vmahesh7829@gmail.com>
2020-10-10 17:25:03 -0700

    renamed historical_fund_data and creating a library to get stock data

--- api_pulls.py
+++ api_pulls.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 from datetime import date
+import pymongo
 
 # the api has a column field that lowers the amount of response data
 
@@ -15,7 +16,7 @@ def genTiingoDict(ticker: str, sDate, eDate):
 
 
     reqBody = "https://api.tiingo.com/tiingo/daily/"+ticker+'/prices?startDate='
-    reqBody = reqBody +sDate+ '&endDate=' + eDate+token+"&columns=adjClose"
+    reqBody = reqBody +sDate+ '&endDate=' + eDate+token+"&columns=adjClose,close"
 
     headers = {
         'Content-Type': 'application/json'
@@ -25,25 +26,37 @@ def genTiingoDict(ticker: str, sDate, eDate):
     pData = requestResponse.json()
 
     stock_Dict = {}
+    closeDict = {}
 
     for currDay in pData:
 
         in_date = currDay['date']
         in_date = date(int(in_date[0:4]),int(in_date[5:7]),int(in_date[8:10]))
-
         stock_Dict[in_date] = currDay['adjClose']
 
+        closeDict[currDay['date'][0:10]]= currDay['close']
 
-    return stock_Dict
+        # also save close price to database
+
+
+    return (stock_Dict,closeDict)
 
 def getStockDict(stockSet: set,sDate,eDate):
+    client = pymongo.MongoClient("mongodb+srv://DyAQ0qSyAdi1Udg9:DyAQ0qSyAdi1Udg9@apidatabank.drr3k.mongodb.net/historicalStockPrices?retryWrites=true&w=majority")
+    db = client.historicalStockPrices
+    posts = db.tiingoClose
 
     sDate = sDate.isoformat()
     eDate = eDate.isoformat()
 
     stock_dict = {}
+    dbDict = {}
 
     for stock in stockSet:
-        stock_dict[stock] = genTiingoDict(stock,sDate,eDate)
+        out = genTiingoDict(stock,sDate,eDate)
+        stock_dict[stock] = out[0]
+        dbDict[stock] = out[1]
 
+    post_id = posts.insert_one(dbDict).inserted_id
+    print(dbDict)
     return stock_dict
--- stockDataLib.py
+++ stockDataLib.py
@@ -5,108 +5,18 @@ from datetime import date
 import pandas_datareader as pdr
 from api_pulls import *
 
-def newGetStockHash(t_data,all_stocks):
+
+def getStockHash(t_data,all_stocks):
     start_date = t_data[0].date
     end_date = t_data[-1].date
 
     if (end_date.year == date.today().year):
         end_date = date.today()
 
-    stockDict = getStockDict(all_stocks,start_date,end_date)
 
+    stockDict = getStockDict(all_stocks,start_date,end_date)
     return stockDict
 
-    # call tiingo api. (send ticker-list start date and end date)
-    # hash the json response and get a list of all the trading days
-    # return the trade day list and the stock price dictionary
-
-# pulls closing price data from yahoo fianance
-def get_close_yahoo(stock_list, start, end):
-    df= pdr.get_data_yahoo(stock_list, end=end, start=start) #grab data from yahoo
-    close= df['Close'] #isolate the close figures
-    # print(close) #pringting close data to the console
-    return close
-
-# pulls adjusted closing price data from yahoo finance
-def get_adj_close_yahoo(stock_list, start, end):
-    df= pdr.get_data_yahoo(stock_list, end=end, start=start) #grab data from yahoo
-    adjclose= df['Adj Close'] #isolate the close figures
-    # print(close) #pringting close data to the console
-    return adjclose
-
-def gen_hist_stockdf(all_trades):
-
-    # to convert the timestamp to a python datetime:
-    # master_df.index = map(lambda x: x.date(), master_df.index)
-    first_day = "2016-01-01"
-    last_day = "2020-08-25"
-
-    ticker_set = set()
-
-    for ticker in all_trades:
-        if ticker[0] not in ticker_set:
-            ticker_set.add(ticker[0])
-
-    V_col = get_adj_close_yahoo('V',first_day,last_day)
-    master_df = pd.DataFrame(V_col)
-    master_df = master_df.rename(columns={"Adj Close": "V"})
-
-    for ticker in ticker_set:
-        next_col = get_adj_close_yahoo(ticker,first_day,last_day)
-        master_df[ticker] = next_col
-
-    master_df.to_parquet('master_df.parquet.gzip',compression='gzip')
-
-
-    df_from_memory = pd.read_parquet('master_df.parquet.gzip')
-
-
-
-# find all the stock data that's in the database and load it to the dict.
-# make API calls for the rest
-def gen_stock_hash(master_df,trade_days):
-
-    master_stock_dict = {}
-    ind_list = master_df.index.tolist()
-
-    for (columnName, columnData) in master_df.iteritems():
-        master_stock_dict[columnName] = {}
-        for day in trade_days:
-            master_stock_dict[columnName][day] = master_df[columnName][day]
-
-    return master_stock_dict
-
-
-
-def create_numpy_stockpricearray():
-    trans_list = parseTDtransactions()
-    master_df = pd.read_parquet('master_df.parquet.gzip')
-
-    trade_days = get_trade_days(trans_list,master_df)
-
-
-        #print(trans.date,trans.name, trans.ticker, trans.amount)
-    #print(stocks_held)
-    trade_days = np.array(trade_days)
-
-    stock_arr = pd.DataFrame(master_df).to_numpy()
-    stock_arr = stock_arr.transpose()
-
-    index_arr = master_df.index
-
-    first_ind = binary_search(index_arr,trade_days[0])
-    last_ind = binary_search(index_arr,trade_days[-1])
-    nump_inds = (first_ind,last_ind)
-
-
-    #print(master_df)
-    #print(stock_arr)
-    #print(master_df)
-    numpy_dict = {}
-    for i in range (len(master_df.columns)):
-        numpy_dict[master_df.columns[i]] = i
-
-    return(trans_list,trade_days,stock_arr,numpy_dict,nump_inds)
 
 # MAIN:
 
