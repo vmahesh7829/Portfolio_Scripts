a55723b28ac142ceccc6475917f053f3d7151fdd
Gianluca C <gianluca.casapietra@gmail.com>
2021-01-18 10:11:51 -0500

    IBKR - fixed a ton of bugs

--- parseIBKR.py
+++ parseIBKR.py
@@ -28,8 +28,12 @@ class Transaction:
         self.ticker= None
         # date
         self.date= None
+        # time
+        self.time= None
         # currency
         self.currency= None
+        # interest
+        self.interest= None
 
 
 if __name__ == "__main__":
@@ -43,7 +47,7 @@ if __name__ == "__main__":
     # for 2019
     # header=None makes no header, just 0-N
     # index_col=0 makes the first column the index values (e.g. Trades, ...)
-    df= pd.read_csv(gcPath2019, header=None, index_col=0)
+    df= pd.read_csv(gcPath2020, header=None, index_col=0)
     
     
     # GETTING TRADE DATA
@@ -57,13 +61,17 @@ if __name__ == "__main__":
     for i in range(dim[0]):
 
         row= trades.iloc[i] # isolating a row for analysis
-        order= row['DataDiscriminator'] # !!! WHEN YOU ILOC A ROW, HEADER BECOMES INDEX !!!!!
+        order= row['DataDiscriminator'] # !!! WHEN YOU ILOC A ROW, HEADER OF DATAGRAME BECOMES INDEX !!!!!
 
         if order == 'Order': # if it's an order we'll continue
 
             ticker= row['Symbol'] # getting the ticker
-            quantity= row['Quantity'] # getting the number of shares traded
-            date= row['Date/Time'] # getting the date & time of trade
+            quantity0= row['Quantity'].replace(',','') # want to remove and commas if CSV adds them for thousands
+            quantity= float(quantity0) # getting the number of shares traded
+            dateTime= row['Date/Time'] # getting the date & time of trade: YYYY-MM-DD, HH:MM:SS (military time)
+
+            date= dateTime.split(', ')[0] # separating out the date
+            time= dateTime.split(', ')[1] # separating out the time
 
             # ATTACH ALL THE NECESSARY DATA TO THE INSTANCE 
             tradeInstance= Transaction('Trade') # INITIALIZING THE INSTANCE
@@ -71,6 +79,7 @@ if __name__ == "__main__":
             tradeInstance.ticker= ticker
             tradeInstance.dShares= quantity
             tradeInstance.date= date
+            tradeInstance.time= time
             tradeInstance.comm= row['Comm/Fee'] # getting the comissions and fees
             tradeInstance.tPrice= row['T. Price'] # getting the transaction price
             tradeInstance.currency= row['Currency'] # getting the currency of the trade
@@ -78,41 +87,39 @@ if __name__ == "__main__":
             # EXTRA DATA POINTS FOR POSSIBLE FUTURE USE
             asset= row['Asset Category'] # grabbing the asset cateogy (stock, ...)
 
-
             if ticker in activityLedger:
 
-                prevTotal= activityLedger[ticker][-1][1].endShares #previous transaction endShares
+                prevTotal= activityLedger[ticker][-1][2].endShares #previous transaction endShares
                 newTotal= prevTotal + quantity # adding traded shares (+/-) to old total to get new total
                 tradeInstance.endShares= newTotal # adding the new total to the instance
 
-                activityLedger[ticker].append((date, tradeInstance)) # adding tuple (date,inst) to list
+                activityLedger[ticker].append((date, asset, tradeInstance)) # adding tuple (date,inst) to list
             
             else: # if first time seeing stock need to initialize list first
                 activityLedger[ticker]=[] # initiallizing a list for a stock (will hold trades)
                 tradeInstance.endShares= quantity # since it's first order, the amount 
-                activityLedger[ticker].append((date, tradeInstance)) # adding tuple (date,inst) to list
+                activityLedger[ticker].append((date, asset, tradeInstance)) # adding tuple (date,inst) to list
 
-    print('Activity ledger with trades')
-    print(activityLedger)
-    
+
     # GETTING DIVIDEND DATA
     dividends= df.loc['Dividends'] # getting everything that is a dividend
-    print(dividends)
+    #print(dividends)
     newHeader= dividends.iloc[0] # assigning first row as headers (which they are in CSV)
     dividends= dividends.iloc[1:] # making DF w/o the first row
     dividends.columns= newHeader # assigning that header to new DF
     dim= dividends.shape # dimensions (rows,cols) which allows for indexing
 
+    
     # LOOP THROUGH ALL DIVIDENDS TO GET THE DATA
     for i in range(dim[0]):
         
-        row=dividends.iloc[0] # isolating a row for analysis (remember, this is now a series w/ header as Index)
+        row=dividends.iloc[i] # isolating a row for analysis (remember, this is now a series w/ header as Index)
         date= row['Date']
 
-        if date != 'NaN': # if there is no date, it is not a dividend (I think this is okay but may BREAK)
+        if type(date) != type(0.1): # if there is no date, it is not a dividend (I think this is okay but may BREAK)
             ticker0= row['Description'] # getting the description which includes ticker
             ticker= ticker0.split('(')[0] # the ticker has a bunch of stuff in parenthesis e.g. AAPL(US485848584)
-            amount= row['Amount'] # the amount of money deposited
+            amount= float(row['Amount']) # the amount of money deposited
             curr= row['Currency'] # the currentcy of dividend payment
             
             divInstance= Transaction('Dividend') # initializing the instance for the dividend
@@ -124,16 +131,80 @@ if __name__ == "__main__":
 
             if 'Dividends' in activityLedger:
                 
-                activityLedger['Dividends'].append((date, divInstance))
+                activityLedger['Dividends'].append((date, ticker, divInstance))
             
             else: 
                 activityLedger['Dividends']=[]
-                activityLedger['Dividends'].append((date, divInstance))
+                activityLedger['Dividends'].append((date, ticker, divInstance))
+
+    #print('Activity ledger with dividends too')
+    #print(activityLedger)
+
+    # GETTING INTEREST DATA
+    interest= df.loc['Interest'] # getting everything that is interest
+    #print(interest)
+    newHeader= interest.iloc[0] # assigning first row as headers (which they are in CSV)
+    interest= interest.iloc[1:] # making DF w/o the first row
+    interest.columns= newHeader # assigning that header to new DF
+    dim= interest.shape # dimensions (rows,cols) which allows for indexing
+
+    # LOOP THROUGH ALL DIVIDENDS TO GET THE DATA
+    for i in range(dim[0]):
+        
+        row=interest.iloc[i] # isolating a row for analysis (remember, this is now a series w/ header as Index)
+        date= row['Date']
+
+        if type(date) != type(0.1): # if there is no date, it is not a dividend (I think this is okay but may BREAK)
+            amount= float(row['Amount']) # the amount of money deposited
+            print(amount)
+            curr= row['Currency'] # the currentcy of dividend payment
+            
+            intInstance= Transaction('Interest') # initializing the instance for the dividend
+            
+            intInstance.interest= amount
+            intInstance.currency= curr
+            intInstance.date= date
+
+            if 'Interest' in activityLedger:
+                
+                activityLedger['Interest'].append((date, intInstance))
+            
+            else: 
+                activityLedger['Interest']=[]
+                activityLedger['Interest'].append((date, intInstance))
 
 
-    print('Activity ledger with dividends too')
-    print(activityLedger)
+    #print('Activity ledger with interest too')
+    #print(activityLedger)
 
+
+    print()
+    print()
+    print('checking over trades: ')
+    test= activityLedger['USD.SEK']
+    for i in test:
+        #print(i)
+        print(i[0], i[2].date, i[2].time, i[2].ticker, i[2].dShares, i[2].endShares)
+
+
+
+    print()
+    print()
+    print('checking over dividends: ')
+    test= activityLedger['Dividends']
+    for i in test:
+        #print(i)
+        print(i[0],i[2].date, i[2].ticker, i[2].divValue)
+
+
+
+    print()
+    print()
+    print('checking over interest: ')
+    test= activityLedger['Interest']
+    for i in test:
+        #print(i)
+        print(i[0],i[1].date, i[1].interest, i[1].currency)
 
 
 
