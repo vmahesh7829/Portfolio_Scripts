fc131ca52527ee82c7fb4047fd8c051a7e998b9e
Viswesh Mahesh <vishy@Visweshs-MacBook-Air.local>
2021-01-18 15:52:06 -0800

    added a function that gets all the data from tiingo

--- api_pulls.py
+++ api_pulls.py
@@ -13,31 +13,19 @@ import cProfile
 import re
 
 
-# IQfeed could be a good source for 100 dollar per month
-# marketstack is 10 dollars per month and 10k request per month (sourced from tiingo)
-# https://eodhistoricaldata.com/ 20 permonth. Can basically get 10k from API as well
-# online people say its sourced from yahoo finance
+# USAGE:
+# stockList is a list of tickers
+# sDate and eDate are iso format strings
+# this outputs a dictionary with ticker as a key
+# value is a dictionary with key date val price
 
-# make sure to check for old tickers first
+# also have this output a list of trading days
+# I want to change so that stockList is a tuplewith (ticker,sDate,eDate)
+# 2016-05-16 2020-07-30
 
-# the api has a column field that lowers the amount of response data
-
-
-# Takes one ticker and a daterange and calls the Tiingo API
-# Returns a dictionary with date as the key and adjusted close
-# as the value for the inputted daterange
-
-
-# using the library is waay faster. Probably because only 1 auth is needed
-# bad part of the library is that it sends everything and we may just want close
 def tiingoMulti(stockList,sDate,eDate):
 
-    # needs a list of tickers, a startdate, and an end date
-    # that list should be a dictionary
-    # that holds the first buy and last sell date
-
-    #sDate and eDate should be converted into iso here
-
+    # thread function spawns a thread that gets data for a stock
     def thread_function(name,client,stock):
         historical_prices = client.get_ticker_price(stock,
                                                 fmt='json',
@@ -67,20 +55,85 @@ def tiingoMulti(stockList,sDate,eDate):
     # pass it in via a configuration dictionary.
     config['api_key'] = "a6051dc9e9d1140d8322de2b99755165d84f9671"
 
-    # Initialize
+    # client is the connection to Tiingo
     client = TiingoClient(config)
 
 
     threads = list()
     stockDictOut = {}
+    dateList = []
 
+    # open a thread that adds apple to the stockdict
+    # populate dateList with the apple trading days in order
+
+    # make concurrent calls to Tiingo API to get stock prices
     for index,stock in enumerate(stockList):
         print("Main    : create and start thread %d.", index)
         x = threading.Thread(target=thread_function, args=(index,client,stock,))
         threads.append(x)
         x.start()
 
+    # close up the threads
     for index, thread in enumerate(threads):
         thread.join()
 
     return stockDictOut
+
+# get all the data tiingo provides
+def tiingoListAllData(stockList,sDate,eDate):
+
+    # thread function spawns a thread that gets data for a stock
+    def thread_function(name,client,stock):
+        historical_prices = client.get_ticker_price(stock,
+                                                fmt='json',
+                                                startDate=sDate,
+                                                endDate=eDate,
+                                                frequency='daily')
+
+        closeDict = {}
+        for currDay in historical_prices:
+
+            in_date = currDay['date']
+
+            # set date key to be a date object
+            in_date = date(int(in_date[0:4]),int(in_date[5:7]),int(in_date[8:10]))
+            closeDict[in_date] = currDay
+
+        nonlocal stockDictOut
+        stockDictOut[stock] = closeDict
+        print("Call %s: finished", name)
+
+    config = {}
+
+    # To reuse the same HTTP Session across API calls (and have better performance), include a session key.
+    config['session'] = True
+
+    # If you don't have your API key as an environment variable,
+    # pass it in via a configuration dictionary.
+    config['api_key'] = "a6051dc9e9d1140d8322de2b99755165d84f9671"
+
+    # client is the connection to Tiingo
+    client = TiingoClient(config)
+
+
+    threads = list()
+    stockDictOut = {}
+    dateList = []
+
+    # open a thread that adds apple to the stockdict
+    # populate dateList with the apple trading days in order
+
+    # make concurrent calls to Tiingo API to get stock prices
+    for index,stock in enumerate(stockList):
+        print("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(index,client,stock,))
+        threads.append(x)
+        x.start()
+
+    # close up the threads
+    for index, thread in enumerate(threads):
+        thread.join()
+
+    return stockDictOut
+
+print(tiingoListAllData(['AAPL','JPM'],'2016-05-16', '2020-07-30'))
